7824a6499ace6cc38f5e5f443e1625b908fe807a
mjaumi <mjaumi2864@gmail.com>
2022-06-09 18:18:16 +0600

    log in component design done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import './Login.css';
+import bg from '../../images/login-bg.png';
+import { Button, Checkbox, Input } from 'antd';
 
 const Login = () => {
 
     // rendering login component here
     return (
-        <div>
-            <h1>This is login</h1>
-        </div>
+        <section className='login'>
+            <div className='form-container'>
+                <h1 className='form-title'>Welcome Back</h1>
+                <p className='form-text'>Sub-title text goes here</p>
+                <div className='form-wrapper'>
+                    <form>
+                        <Input className='input' placeholder='Email Address *' type='email' required />
+                        <Input className='input' placeholder='Password *' type='password' required />
+                        <Button className='form-login-btn btn' type='primary' size='large' block>Login</Button>
+                    </form>
+                    <div className='form-footer'>
+                        <Checkbox className='form-footer-text'>Remember Password</Checkbox>
+                        <Button className='form-footer-text forgot-btn' type="link">Forgot Password?</Button>
+                    </div>
+                </div>
+            </div>
+            <div className='img-container'>
+                <img src={bg} alt="login bg" />
+            </div>
+        </section>
     );
 };
 
